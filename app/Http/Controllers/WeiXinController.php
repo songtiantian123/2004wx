323fc2ecf351520b54ede931cec7e4671559aeb7
songtiantian <1968551623@qq.com>
2020-11-06 12:10:11 +0800

    微信接入

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 
 class WeiXinController extends Controller
 {
-    public function wx()
-{
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];
+    public function checkSignature(Request $request)
+    {
+        $echostr=$request->echostr;
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
 
-    $token = env('WX_TOKEN');
-    $tmpArr = array($token, $timestamp, $nonce);
-    sort($tmpArr, SORT_STRING);
-    $tmpStr = implode( $tmpArr );
-    $tmpStr = sha1( $tmpStr );
+        $token = env('WX_TOKEN');
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
 
-    if( $tmpStr == $signature ){
-
-        echo $_GET['echostr'];
-    }else{
-        echo '失败';
+        if( $tmpStr == $signature ){
+            echo $echostr;
+        }else{
+            return false;
+        }
     }
-}
 }
