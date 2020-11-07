ffa24150ace2e51341fd4287a848c0c515158575
songtiantian <1968551623@qq.com>
2020-11-07 11:19:15 +0800

    5

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
-/**php首页 */
-Route::get('/info', function () {
-    echo phpinfo();
-});
-Route::get('/hello', function () {
-    echo 'hello wx';
-});
-Route::get('/text','TextController@text');// redis测试
-Route::get('/text1','TextController@text1');// 测试1
+///**php首页 */
+//Route::get('/info', function () {
+//    echo phpinfo();
+//});
+//Route::get('/hello', function () {
+//    echo 'hello wx';
+//});
+//Route::get('/text','TextController@text');// redis测试
+//Route::get('/text1','TextController@text1');// 测试1
 
 //Route::post('/wx','WeiXinController@checkSignature');// 微信接口
 Route::match(['get','post'],'/wx','WeiXinController@wxEvent');// 接收事件推送
